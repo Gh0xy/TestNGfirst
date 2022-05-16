f37b79d0f01e51dbf208b62303d969c2c330666d
Gh0xy <gh0xy@pm.me>
2022-05-16 23:18:51 +0200

    First commit

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,7 +1,16 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.stream.DoubleStream;
+
+public class Calculator {
+
+    public static double add(double... operands) {
+        return DoubleStream.of(operands)
+                .sum();
+    }
+
+    public static double multiply(double... operands) {
+        return DoubleStream.of(operands)
+                .reduce(1, (a, b) -> a * b); // change 1 to 2 for multiplication bug
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class CalculatorTest {
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.example.Calculator;
+
+public class CalculatorTest {
+
+    @Test
+    void add() {
+        Assert.assertEquals  (4, Calculator.add(2, 2));
+    }
+
+    @Test
+    public void multiply() {
+                Assert.assertEquals(4, Calculator.multiply(2, 2));
+                Assert.assertEquals(-4, Calculator.multiply(2, -2));
+                Assert.assertEquals(4, Calculator.multiply(-2, -2));
+                Assert.assertEquals(0, Calculator.multiply(1, 0));
+    }
 }
